4c96a2f3aaf15e502cf67fe0edbc27a7a58c5a3e
Alexandr Pushkin <118882770+push-ab@users.noreply.github.com>
2024-07-19 16:14:27 +0400

    Update task.js
    avg = Math.round(avg * 100.0) / 100.0;

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,8 +5,9 @@ function getArrayParams(...arr) {
   let avg = 0;
 
   sum = arr.reduce((acc, val) => acc + val);
-
   avg = sum / arr.length;
+  avg = Math.round(avg * 100.0) / 100.0;
+
 
   return { min: min, max: max, avg: avg };
 }
