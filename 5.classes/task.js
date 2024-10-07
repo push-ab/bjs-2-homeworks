eefc23c102b025774fa8d64a6a6a3188fe1c6377
Alexandr Pushkin <pushkin-ab-tecnocentr@yandex.ru>
2024-10-07 11:10:03 +0400

    [main] -сделал дз классы

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,9 +1,61 @@
  class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-     this.Name = name;
-     this.Release = releaseDate;
-     this.PagesCount = pagesCount;
-     this.State = 100;
-     this.Type = null;
-    }
+     constructor(name, releaseDate, pagesCount) {
+         this.name = name;
+         this.releaseDate = releaseDate;
+         this.pagesCount = pagesCount;
+         this.state = 100;
+         this.type = null;
+     }
+
+     fix() {
+         this.state *= 1.5;
+     }
+
+     set state(value) {
+         if (value < 0) {
+             this._state = 0;
+         } else if (value > 100) {
+             this._state = 100;
+         } else {
+             this._state = value;
+         }
+     }
+
+     get state() {
+         return this._state;
+     }
+ }
+
+ class Library {
+     constructor(name) {
+         this.name = name;
+         this.books = [];
+     }
+
+     addBook(book) {
+         if (book.state > 30) {
+             this.books.push(book);
+         }
+     }
+
+     findBookBy(type, value) {
+         for (let i = 0; i < this.books.length; i++) {
+             if (this.books[i][type] === value) {
+                 return this.books[i];
+             }
+         }
+
+         return null;
+     }
+
+     giveBookByName(bookName) {
+         for (let i = 0; i < this.books.length; i++) {
+             if (this.books[i].name === bookName) {
+                 this.books.splice(i, 1);
+                 return this.books[i];
+             }
+         }
+
+         return null;
+     }
  }
