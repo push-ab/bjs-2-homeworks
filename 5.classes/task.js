ed83bf868a5638b33d4a2ab95d605fe008a3167a
Alexandr Pushkin <pushkin-ab-tecnocentr@yandex.ru>
2024-10-08 09:44:50 +0400

    [main] -правки

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -4,7 +4,6 @@
          this.releaseDate = releaseDate;
          this.pagesCount = pagesCount;
          this.state = 100;
-         this.type = null;
      }
 
      fix() {
@@ -23,6 +22,42 @@
 
      get state() {
          return this._state;
+     }
+ }
+
+ class Magazine extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount)
+        this.type = 'magazine'
+    }
+ }
+
+ class Book extends PrintEditionItem {
+     constructor (name, releaseDate, pagesCount, author) {
+         super(name, releaseDate, pagesCount)
+         this.type = 'book';
+         this.author = author
+     }
+ }
+
+ class NovelBook extends Book {
+     constructor (name, releaseDate, pagesCount, author) {
+         super(name, releaseDate, pagesCount, author)
+         this.type = 'novel';
+     }
+ }
+
+ class FantasticBook extends Book {
+     constructor (name, releaseDate, pagesCount, author) {
+         super(name, releaseDate, pagesCount, author)
+         this.type = 'fantastic';
+     }
+ }
+
+ class DetectiveBook extends Book {
+     constructor (name, releaseDate, pagesCount, author) {
+         super(name, releaseDate, pagesCount, author)
+         this.type = 'detective';
      }
  }
 
