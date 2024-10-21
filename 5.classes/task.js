029930daada6e57479ed3b41fe3343d5c7c3c002
Alexandr Pushkin <pushkin-ab-tecnocentr@yandex.ru>
2024-10-21 22:31:33 +0400

    [main] - дебаг выдачи книги5 ytn yt yfotk

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -87,8 +87,8 @@ class PrintEditionItem {
      giveBookByName(bookName) {
          for (let i = 0; i < this.books.length; i++) {
              if (this.books[i].name === bookName) {
-                 this.books.splice(i, 1);
-                 return this.books[1];
+                 const [book] = this.books.splice(i, 1);
+                 return book;
 
              }
          }
