96ff7b9d0b20b887c4b592476880b9c246bf9a23
Alexandr Pushkin <pushkin-ab-tecnocentr@yandex.ru>
2024-11-19 19:54:13 +0400

    [main]- домашка начало

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,9 @@
-﻿
+﻿function parseCount(value){
+    let parsedValue = Number.parseInt(value);
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    console.log(parsedValue);
+    return parsedValue;
+
+}
